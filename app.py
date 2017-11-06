8c6142b194d6bb1e50db0f67a9b735a8c5f2d2f8
Matthew Schultz <mschult7@binghamton.edu>
2017-11-06 21:18:02 +0000

    removed method in app.py

--- app.py
+++ app.py
@@ -8,14 +8,7 @@ def index():
 	x = int(round(time.time() * 1000))
 	return render_template("index.html", stylevar=x)
 
-@app.route("/learning-templates", methods=["GET", "POST"])
-def learning_templates():
-    if request.method == "POST":
-        print(request.data)
-        print(request.form)
-    a = 2**7
-    username = "steve"
-    return render_template("learning_templates.html", answer=a, greeting=username)
+
 
 
 if __name__ == "__main__":
